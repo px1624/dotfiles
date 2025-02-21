c1f1476f097825e1a30a9b2753cf62c4b6caa98d
px1624 <pengdaxie123@gmail.com>
2025-02-20 23:40:30 -0800

    fix autocmd error

--- nvim/.config/nvim/lua/config/autocmd.lua
+++ nvim/.config/nvim/lua/config/autocmd.lua
@@ -3,6 +3,6 @@ vim.api.nvim_create_autocmd('VimEnter', {
   group = vim.api.nvim_create_augroup('vim_startup', { clear = true }),
   callback = function()
     local cur_path = vim.fn.expand('%:p:h')
-    vim.cmd('cd' .. cur_path)
+    vim.cmd('cd ' .. cur_path)
   end
 })
